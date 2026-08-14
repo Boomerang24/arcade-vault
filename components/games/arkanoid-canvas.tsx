94c7fa93acbb5663764a447d3a0357485702d6ee
Boomerang24 <alexcomoed@gmail.com>
2026-08-13 21:23:13 -0600

    refactor: fixed arkanoid canvas width

--- components/games/arkanoid-canvas.tsx
+++ components/games/arkanoid-canvas.tsx
@@ -34,6 +34,18 @@ export const ArkanoidCanvas = forwardRef<ArkanoidCanvasHandle, Props>(
       reset: () => engineRef.current?.reset(),
       forceGameOver: () => engineRef.current?.forceGameOver(),
     }));
-    return <canvas ref={canvasRef} width={800} height={600} />;
+    return (
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={600}
+        style={{
+          position: "absolute",
+          inset: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    );
   },
 );
